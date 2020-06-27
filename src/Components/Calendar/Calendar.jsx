d16d1a1919b879d5012713245aaac3001dea2039
Rohitkumar1105 <rohit.kv11@gmail.com>
2020-06-27 23:30:49 +0530

    added the fullcalendar library

---
+++ src/Components/Calendar/Calendar.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+
+const Calendar = () => {
+    return (
+        <div>
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                    headerToolbar={{
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                    }}
+                events={[
+                    { title: 'event 1', date: '2019-04-01' },
+                    { title: 'event 2', date: '2019-04-02' }
+                ]}
+            />
+        </div>
+    )
+}
+
+export default Calendar
+
+// import React from 'react'
+// import FullCalendar, { formatDate } from '@fullcalendar/react'
+// import dayGridPlugin from '@fullcalendar/daygrid'
+// import timeGridPlugin from '@fullcalendar/timegrid'
+// import interactionPlugin from '@fullcalendar/interaction'
+// // import { INITIAL_EVENTS, createEventId } from './event-utils'
+
+// export default class DemoApp extends React.Component {
+
+//   state = {
+//     weekendsVisible: true,
+//     currentEvents: []
+//   }
+
+//   render() {
+//     return (
+//       <div className='demo-app'>
+//         <div className='demo-app-main'>
+//           <FullCalendar
+//             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+//             headerToolbar={{
+//               left: 'prev,next today',
+//               center: 'title',
+//               right: 'dayGridMonth,timeGridWeek,timeGridDay'
+//             }}
+//             initialView='dayGridMonth'
+//             editable={true}
+//             selectable={true}
+//             selectMirror={true}
+//             dayMaxEvents={true}
+//             weekends={this.state.weekendsVisible}
+//             // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+//             // select={this.handleDateSelect}
+//             // eventContent={renderEventContent} // custom render function
+//             eventClick={this.handleEventClick}
+//             eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+//             /* you can update a remote database when these fire:
+//             eventAdd={function(){}}
+//             eventChange={function(){}}
+//             eventRemove={function(){}}
+//             */
+//           />
+//         </div>
+//       </div>
+//     )
+//   }
+// }
