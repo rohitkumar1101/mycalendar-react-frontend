95f03030a8486368124de45479d204a9a33adcee
Rohit Kumar <rohit@thinkjs.club>
2022-03-24 15:55:07 +0530

    dummy things added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Calendar } from './Components/index'
+import { Calendar, Text } from './Components'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path={`${process.env.PUBLIC_URL}/`} component={Calendar} />
+        <Route exact path={`${process.env.PUBLIC_URL}/hbd`} component={Text} />
       </Switch>
     </BrowserRouter>
   );
